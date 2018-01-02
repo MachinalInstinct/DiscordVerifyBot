68c62341a6c5d318bcbe757db5519d3d06ca7130
jensolaf <jens@animal.ee>
2018-01-03 00:11:20 +0200

    Latest changes

--- cogs/background.py
+++ cogs/background.py
@@ -7,17 +7,22 @@ import cogs.basic.rcon as rcon
 import asyncio
 
 
+
+
+
 class Background:
     def __init__(self, bot):
         self.bot = bot
+        self.runvip = True
         bot.bg_task = bot.loop.create_task(self.vip())
 
     async def vip(self):
-        #await self.wait_until_ready()
-        #print(rcon.get_vip_steamid_list())
-        print(rcon.get_vip_steamid_list())
-        #await asyncio.sleep(10)
-        await asyncio.sleep(300)  # task runs every 5min
+        while True:
+            #print(self.runvip)
+            print(rcon.get_vip_steamid_list())
+            await asyncio.sleep(300)  # task runs every 5min
+
+
 
 
 def setup(bot):
--- cogs/basic/rcon.py
+++ cogs/basic/rcon.py
@@ -3,12 +3,12 @@ import json
 import sys
 
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-ws = create_connection("ws://rust.setomaa.win:28016/MinaSiin")
 name = "Peko"
 
 
 def rcon(msg):
     try:
+        ws = create_connection("ws://rust.setomaa.win:28016/MinaSiin")
         d = {
             "Message": str(msg),
             "Identifier": 0,
@@ -20,10 +20,12 @@ def rcon(msg):
         ws.send(json_info)
         raw = ws.recv()
         #print(raw)
-        ws.close()
+        #ws.close()
         result = json.loads(raw)
         #print(type(result))
         message = result['Message']
+        #print(message)
+        ws.close()
         return message
     except Exception as e:
         raise e
@@ -34,8 +36,14 @@ def get_oxide_group_users(group):
         raw = rcon("oxide.show group "+str(group))
         list_str = raw.splitlines()[1]
         raw_list = list_str.split(",")
+        count = 0
         for entry in raw_list:
-            id_list.append(entry.split(' (', 1)[0])
+            userid1 = entry.split(' (', 1)[0]
+            userid2 = userid1
+            if count > 0:
+                userid2 = userid1.split(' ')[1]
+            id_list.append(userid2)
+            count = count+1
 
         return id_list
 
@@ -45,9 +53,7 @@ def get_oxide_group_users(group):
 
 def get_vip_steamid_list():
     try:
-        print("test")
         vips = get_oxide_group_users("p2w")
-
         return vips
     except Exception as e:
         raise e
